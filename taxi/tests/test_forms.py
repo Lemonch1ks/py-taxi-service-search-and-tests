ee4fbfa3a125ea706ee04f115b7a7b549dd5e238
Oleksandr Dvirko <dvalex02@gmail.com>
2026-06-09 20:33:59 +0300

    created form tests

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from taxi.forms import CarSearchForm, DriverSearchForm, ManufacturerSearchForm
 from taxi.models import Manufacturer, Car
 
 
@@ -11,35 +10,52 @@ class TestSearch(TestCase):
         self.user = get_user_model().objects.create_user(
             username="test_user",
             password="test_password",
-            license_number="test_license_number"
+            license_number="ABC12345",
         )
+        self.client.force_login(self.user)
 
     def test_search_cars_by_model(self):
-        honda = Manufacturer.objects.create(name="Honda", country="Japan")
-        honda_civic = Car.objects.create(manufacturer=honda, model="Honda Civic")
+        honda = Manufacturer.objects.create(
+            name="Honda",
+            country="Japan",
+        )
+        Car.objects.create(
+            manufacturer=honda,
+            model="Honda Civic",
+        )
 
-        toyota = Manufacturer.objects.create(name="Toyota", country="Japan")
-        toyota_carola = Car.objects.create(manufacturer=toyota, model="Toyota Carola")
+        toyota = Manufacturer.objects.create(
+            name="Toyota",
+            country="Japan",
+        )
+        Car.objects.create(
+            manufacturer=toyota,
+            model="Toyota Corolla",
+        )
 
         response = self.client.get(
             reverse("taxi:car-list"),
-            {"car_model": "Corolla"},
+            data={"car_model": "Corolla"},
         )
+
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Toyota Corolla")
         self.assertNotContains(response, "Honda Civic")
 
-
     def test_search_drivers_by_username(self):
-        test_data = {
-            "username": "Test User1",
-        }
-        form = DriverSearchForm(data=test_data)
-        self.assertEqual(form.is_valid(), True)
+        response = self.client.get(
+            reverse("taxi:driver-list"),
+            data={"username": "test_user"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "test_user")
 
     def test_search_manufacturer_by_name(self):
-        test_data = {
-            "manufacturer_name": "Test Manufacturer1",
-        }
-        form = ManufacturerSearchForm(data=test_data)
-        self.assertEqual(form.is_valid(), True)
+        new_manufacturer = Manufacturer.objects.create(
+            name="test_manufacturer",
+        )
+        response = self.client.get(
+            reverse("taxi:manufacturer-list"),
+            data={"manufacturer": "test_manufacturer"},
+        )
